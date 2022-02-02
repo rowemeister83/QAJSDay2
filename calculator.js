e2fd6913e1a852e8faa60bb29a574921d350d628
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-02 17:34:16 +0000

    just need to do output

--- calculator.js
+++ calculator.js
@@ -1,2 +1,54 @@
 `use strict`
 import * as DOM from './calculatordom.js';
+
+
+
+DOM.btnAdd.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.btnSub.onclick = () => sub(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.btnDiv.onclick = () => div(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.btnMult.onclick = () => mult(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.btnEql.onclick = () => disp();
+DOM.btnClear.onclick = () => clear();
+
+function add (num1, num2){
+    let operator = "+";
+    let answer = 0;
+    answer = +num1 + +num2;
+    disp(num, num2, operator, answer);
+}
+
+function sub (num1, num2){
+    let operator = "-";
+    let answer = 0;
+    answer = +num1 - +num2;
+    disp(num, num2, operator, answer);
+}
+
+function div (num1, num2){
+    let operator = "/";
+    let answer = 0;
+    answer = +num1 / +num2;
+    disp(num, num2, operator, answer);
+
+}
+
+function mult (num1, num2){
+    let operator = "*";
+    let answer = 0;
+    answer = +num1 * +num2;
+    disp(num, num2, operator, answer);
+}
+
+
+
+function disp(num1, num2, operator, answer){
+    let child = document.createElement("li");
+    child.innerHTML = num1, num2, operator, answer;
+    DOM.output.appendChild(child);
+}
+function clear()
+{
+    DOM.inputOne.value = "";
+    DOM.inputTwo.value = "";
+    document.getElementById("list_output").innerHTML = "";
+}
--- calculatordom.js
+++ calculatordom.js
@@ -1,1 +1,9 @@
-export const inputOne = document.getElementById(`input_1`);
+export const inputOne = document.getElementById("input_1");
+export const inputTwo = document.getElementById("input_2");
+export const btnAdd = document.getElementById("button_add");
+export const btnSub = document.getElementById("button_subtract");
+export const btnMult = document.getElementById("button_multiply");
+export const btnDiv = document.getElementById("button_divide");
+export const btnEql = document.getElementById("button_equal");
+export const btnClear = document.getElementById("button_clear");
+export const output = document.getElementById("list_output");
